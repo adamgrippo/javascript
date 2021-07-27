13e0660ab1e7ceb2dd37b29f2ab565ef59107153
Taylor <adamgrippo@iCloud.com>
2021-07-27 06:35:01 -0400

    Create fizz-buzz.js

---
+++ fizz-buzz.js
@@ -0,0 +1,8 @@
+for ( let x = 1 ; x <= 100 ; x++ ) {
+	if ( x % 3 == 0 || x % 5 == 0 ) {
+		let output = "" ;
+		if ( x % 3 == 0 ) output += "Fizz" ;
+		if ( x % 5 == 0 ) output += "Buzz" ;
+		console.log ( output, "\n" ) ;
+	} else console.log ( x + "\n" ) ;
+}
